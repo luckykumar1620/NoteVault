49f269e3a4fc8381a7a4c651a3650651951965fc
Lucky Kumar <lucky9110132241@gmail.com>
2025-08-30 04:00:14 +0530

    implement useLocation hook

--- src/components/About.js
+++ src/components/About.js
@@ -2,14 +2,11 @@ import React,{useContext, useEffect} from 'react'
 import noteContext from '../context/notes/NoteContext'
 
 const About = () => {
-  const a=useContext(noteContext);
-  useEffect(()=>{
-    a.update();
-    // eslint-disable-next-line
-  },[])
+
+ 
   return (
     <div>
-      <h1>this is about {a.state.name} and he is  in class {a.state.class}</h1>
+      <h1>this is about page</h1>
     </div>
   )
 }
