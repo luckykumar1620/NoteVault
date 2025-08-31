4b410cad238357e3ad731d8cecb7f256b94fc165
Lucky Kumar <lucky9110132241@gmail.com>
2025-09-01 02:47:42 +0530

    adding signup components

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -17,16 +17,17 @@ router.post('/createuser',
       body("password","password mmust be atleast 5 character").isLength({min:5})
     ],
     async(req,res)=>{  
+      let success=false;
      //if there are error ,return bad request and the error
      const errors=validationResult(req);
      if(!errors.isEmpty()){
-       return res.status(400).json({errors:errors.array()});
+       return res.status(400).json({success,errors:errors.array()});
      }
      try{
      //check wheather the user with this email exists already
      let user=await User.findOne({email:req.body.email})
      if(user){
-        return res.status(400).json({error:"sorry a user with this email already exists"});
+        return res.status(400).json({success,error:"sorry a user with this email already exists"});
      }
      
 
@@ -47,7 +48,8 @@ router.post('/createuser',
     const authToken=jwt.sign(data,JWT_SECRET);
     
     // res.json(user) 
-    res.json({authToken})  
+    success=true;
+    res.json({success,authToken})  
       }catch(error){
         console.error(error.message);
         res.status(500).send("internal server error");
@@ -62,7 +64,7 @@ router.post('/login',
       body("password","password can't be blank").exists()
     ],
     async(req,res)=>{
-      success=false;
+     let success=false;
      //if there are error ,return bad request and the error
      const errors=validationResult(req);
      if(!errors.isEmpty()){
@@ -72,13 +74,14 @@ router.post('/login',
      try {
         const user=await User.findOne({email});
         if(!user){
+          success=false;
            return res.status(400).send({error:"please try to login with correct credentials"});
         }
 
         const passwordCompare=await bcrypt.compare(password,user.password);
         if(!passwordCompare){
-          success=false;
-           return res.status(400).send({successerror:"please try to login with correct credentials"}); 
+         success=false;
+           return res.status(400).send({success,error:"please try to login with correct credentials"}); 
         }
 
         const data={
